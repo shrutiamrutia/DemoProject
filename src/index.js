8af4efd68cd1e78909638458c82a9966c496527b
shrutiamrutia <shrutiamrutiya.albetrios@gmail.com>
2023-06-06 18:29:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import reducer from './store/reducers/reducer'
-import { Provider } from 'react-redux';
 
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import * as ReactDOM from 'react-dom'; import { StrictMode } from "react";
+// import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
 
 
-const store = createStore(reducer, composeEnhancers(
-  applyMiddleware(thunk)
-))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Provider >
+      <App />
+    </Provider>
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-
-
-
-
-
-
